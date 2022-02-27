efbd2d916672c48fdfb254c88e2d621353ff454c
Alexander Kumanov <kumana@abv.bg>
2022-02-27 19:25:30 +0200

    updated settings.py

--- music_app/music_app/settings.py
+++ music_app/music_app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'music_app',
         'USER': 'postgres',
-        'PASSWORD': '901209Method',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
